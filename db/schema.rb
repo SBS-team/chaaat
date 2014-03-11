f79d2bee34605d8403b51f6a65b2098e6dc6cda8
Nataly <natalyasvd@gmail.com>
2014-03-11 16:56:18 +0200

    add model rooms_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311113814) do
+ActiveRecord::Schema.define(version: 20140311142428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,13 @@ ActiveRecord::Schema.define(version: 20140311113814) do
     t.string   "topic"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "user"
+  end
+
+  create_table "rooms_users", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "room_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
