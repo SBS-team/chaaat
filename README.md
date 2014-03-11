78b8074f4363a663d7cca6bda70a24d92970af3d
temchik2004 <temchik2004@gmail.com>
2014-03-11 11:25:29 +0200

    Create model

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 Chat
-=========
 
 This application was generated with the [rails_apps_composer](https://github.com/RailsApps/rails_apps_composer) gem
 provided by the [RailsApps Project](http://railsapps.github.io/).
 
 Diagnostics
------------
 
 This application was built with recipes that are known to work together.
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307144347) do
+ActiveRecord::Schema.define(version: 20140311090927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: true do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.text     "attach_path"
+    t.text     "attach_size"
+    t.integer  "room_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["room_id"], name: "index_messages_on_room_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
+  create_table "rooms", force: true do |t|
+    t.string   "name"
+    t.string   "topic"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
