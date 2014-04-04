f37e78e6c0a1e2c8bc89c2455e12078b5ad8ed72
Nataly <natalyasvd@gmail.com>
2014-04-04 18:22:40 +0300

    merge develop

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -14,8 +14,6 @@ class Devise::SessionsController < DeviseController
     self.resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
-    #User.update(current_user.id, :user_status=>"Available")
-    #Pusher['status'].trigger('change_status', :status=>"Available",:user_id=>current_user.id)
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
   end
