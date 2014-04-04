94e801408c506f5d766ccd8751881ee7e314c3d9
Nataly <natalyasvd@gmail.com>
2014-04-04 18:24:00 +0300

    merge develop

---
+++ spec/models/admin_user_spec.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: admin_users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#
+# Indexes
+#
+#  index_admin_users_on_email                 (email) UNIQUE
+#  index_admin_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
+require 'spec_helper'
+
+describe AdminUser do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
