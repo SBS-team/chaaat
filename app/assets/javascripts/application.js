6f2d7a418c3951ce3b1531586992c7932144fc02
Nataly <natalyasvd@gmail.com>
2014-04-08 13:47:17 +0300

    background admin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery_ujs
 //= require emojify
 //= require jquery-1.10.2.js
