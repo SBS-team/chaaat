d6e9b7d58d61c5215136382fa53fa14606b49591
mpaniotov <elteck81@gmail.com>
2014-04-14 16:42:26 +0300

    new rigth panel

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -15,4 +15,32 @@ $(document).ready(function(){
                 $("#drop1.dropdown-toggle.avail")[0].innerHTML ="<span class=\""+get_user_status_style(msg)+"\"></span>"+msg+"<span class=\"glyphicon glyphicon-hand-down\"></span>";
             });
     });
+
+
+    rightPanel=$('.right_panel')[0].clientHeight;
+    $('.list').css('height',rightPanel-78);
+    window.onresize = function(event) {
+        newHeight=$('.panel-default')[0].offsetTop;
+        $('.list').css('height',newHeight-30);
+        $('.right_search').css('height',$('.panel-default')[0].clientHeight-80);
+    }
+
+    clicked=false
+    $('.collapsed').on('click',function(){
+        rightPanel=$('.right_panel')[0].clientHeight;
+        if (clicked==false){
+            $('.panel-default').css('height','50%');
+            bottomPanel=$('.panel-default')[0].clientHeight;
+            $('.right_search ').css('height',bottomPanel-118);
+            $('.list').css('height',rightPanel/2-30);
+            $('.right_search').css('height',$('.panel-default')[0].clientHeight-80);
+            clicked=true;
+        }
+        else{
+            $('.panel-default').css('height','auto');
+            $('.list').css('height', rightPanel-78);
+            clicked=false;
+        }
+    });
+
 });
