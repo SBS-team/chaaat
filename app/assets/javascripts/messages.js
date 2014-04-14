6e64fa97c9ff94e888ec1bb77e4650eb1a332743
galas <artem.galas@gmail.com>
2014-04-14 16:27:51 +0300

    modifeid message.js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -378,12 +378,5 @@ $(document).ready(function(){
         });
 
     });
-$('body').on('click', function (e) {
-$('[data-toggle="popover"]').each(function () {
-if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
-$(this).popover('hide');
-}
-});
 
-});
 });
