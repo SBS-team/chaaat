b69b47cc20768d5921893c7ffabad064ffe018e5
mpaniotov <elteck81@gmail.com>
2014-04-23 12:22:11 +0300

    update pusher controller

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -2,10 +2,12 @@ class PusherController < ApplicationController
   protect_from_forgery :except => :auth # stop rails CSRF protection for this action
 
   def auth
+    if user_signed_in?
     if current_user && params[:room_id]
       authentication_query=Room.includes(:rooms_users).where('rooms_users.user_id'=>current_user.id,'rooms.id'=>params[:room_id].to_i).exists? && (params[:channel_name] == "private-#{params[:room_id]}" || params[:channel_name] == "private-#{current_user.id}"|| params[:channel_name] == "presence-status")
     else
       authentication_query=params[:channel_name] == "presence-status" || params[:channel_name] == "private-#{current_user.id}"
+    end
     end
       if authentication_query
         auth = Pusher[params[:channel_name]].authenticate(params[:socket_id],:user_id => current_user.id)
