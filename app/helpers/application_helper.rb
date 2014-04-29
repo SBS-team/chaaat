b9945d1f4ad6540296faff535ef6733dc211f094
dmmoroka92 <dmoroka92@gmail.com>
2014-04-29 15:19:54 +0300

    gravatar avatar support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,22 @@
 module ApplicationHelper
   def avatar_url(user, foto_size)
-    #if user.respond_to?('avatar')
-    if user.avatar.present?
-      if foto_size==50
-       user.avatar
-      else
-        user.profile_avatar
-      end
-    else
-      default_url = "#{root_url}images/guest.png"
-      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{foto_size}&d=#{CGI.escape("mm")}"
-    end
+      image_url = Gravatar.new(user.email).image_url :default => "http://cdn2.vox-cdn.com/images/verge/default-avatar.v9899025.gif"
+      return image_url
   end
 
   def get_user_status_style(user_status)
     case user_status
-     when 'Available'
-        'glyphicon glyphicon-eye-open drop-av drop-col-mar'
-     when 'Away'
-        'glyphicon glyphicon-eye-close drop-away drop-col-mar'
-     when 'Do not disturb'
+     when "Available"
+        "glyphicon glyphicon-eye-open drop-av drop-col-mar"
+     when "Away"
+        "glyphicon glyphicon-eye-close drop-away drop-col-mar"
+     when "Do not disturb"
         "glyphicon glyphicon-eye-close drop-dnd drop-col-mar"
      when "Offline"
         "glyphicon glyphicon-eye-close drop-col-mar"
-     when 'Help'
+     when "Help"
         "glyphicon glyphicon-question-sign drop-hlp drop-col-mar"
     end
   end
 end
+
