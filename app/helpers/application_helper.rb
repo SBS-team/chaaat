4d0976044fdebfec600964f1867293b2c5fdc7a1
Viktor Markevich <viktor.markevich@faceit.com.ua>
2014-04-30 11:07:38 +0300

    refactred application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,14 @@
 module ApplicationHelper
   def avatar_url(user, foto_size)
-      image_url = Gravatar.new(user.email).image_url :default => "http://cdn2.vox-cdn.com/images/verge/default-avatar.v9899025.gif"
-      return image_url
+      Gravatar.new(user.email).image_url default: 'http://cdn2.vox-cdn.com/images/verge/default-avatar.v9899025.gif'
   end
 
-  def get_user_status_style(user_status)
-    case user_status
-     when "Available"
-        "glyphicon glyphicon-eye-open drop-av drop-col-mar"
-     when "Away"
-        "glyphicon glyphicon-eye-close drop-away drop-col-mar"
-     when "Do not disturb"
-        "glyphicon glyphicon-eye-close drop-dnd drop-col-mar"
-     when "Offline"
-        "glyphicon glyphicon-eye-close drop-col-mar"
-     when "Help"
-        "glyphicon glyphicon-question-sign drop-hlp drop-col-mar"
-    end
+  def get_user_status_style(status)
+     { :Available => 'glyphicon glyphicon-eye-open drop-av drop-col-mar',
+       :Away => 'glyphicon glyphicon-eye-close drop-away drop-col-mar',
+       :'Do not disturb' => 'glyphicon glyphicon-eye-close drop-dnd drop-col-mar',
+       :Offline => 'glyphicon glyphicon-eye-close drop-col-mar',
+       :Help => 'glyphicon glyphicon-question-sign drop-hlp drop-col-mar' }[status.to_sym]
   end
 end
 
