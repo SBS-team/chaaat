1cf94c31ecf8f151b06c2fff7d3f221fa07ba684
Viktor Markevich <viktor.markevich@faceit.com.ua>
2014-05-05 15:35:18 +0300

    first part refactoring

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -2,35 +2,35 @@ class PusherController < ApplicationController
   protect_from_forgery :except => :auth # stop rails CSRF protection for this action
 
   def auth
-    if current_user && params[:room_id]
-      authentication_query=Room.includes(:rooms_users).where('rooms_users.user_id'=>current_user.id,
-                                                             'rooms.id'=>params[:room_id].to_i).exists? &&
-                                                              (params[:channel_name] == "private-#{params[:room_id]}" ||
-                                                              params[:channel_name] == "private-#{current_user.id}"||
-                                                              params[:channel_name] == "presence-status")
+    authentication_query = if current_user && params[:room_id]
+                              Room.includes(:rooms_users).where( 'rooms_users.user_id' => current_user.id,
+                             'rooms.id' => params[:room_id].to_i ).exists? && ( params[:channel_name] == "private-#{params[:room_id]}" ||
+                                                                                authentication_status )
+                              end || authentication_status                                                         
+    if authentication_query
+      render json: Pusher[params[:channel_name]].authenticate( params[:socket_id], user_id: current_user.id )
     else
-      authentication_query=params[:channel_name] == "presence-status" || params[:channel_name] == "private-#{current_user.id}"
+      render text: 'Not authorized', status: '403'
     end
-      if authentication_query
-        auth = Pusher[params[:channel_name]].authenticate(params[:socket_id],:user_id => current_user.id)
-        render :json => auth
-      else
-        render :text => "Not authorized", :status => '403'
-      end
   end
 
   def stat
-    previous_status=User.find(params[:user_id]).user_status
-    if params[:client_status]=='Available' && previous_status!='Offline'
-      user_status=previous_status
-    elsif params[:client_status]=='Available'
-      user_status='Available'
-    else
-      user_status='Offline'
-    end
-    Pusher['presence-status'].trigger_async('change_status', :status=>user_status,:user_id=>params[:user_id])
-    User.update(params[:user_id], :user_status =>user_status)
-    render :text=>"Success"
+    user = User.find(params[:user_id])
+    user_status = if params[:client_status] == 'Available' && user.user_status != 'Offline'
+                    user.user_status
+                  elsif params[:client_status] == 'Available'
+                    'Available'
+                  end || 'Offline'
+    if user.update( user_status: user_status )
+      Pusher['presence-status'].trigger_async( 'change_status', status: user_status, user_id: params[:user_id] )
+      render text: 'Success'
+    end || ( render text: 'Error' )
+  end
+
+  private
+
+  def authentication_status
+    params[:channel_name] == 'presence-status' || params[:channel_name] == "private-#{current_user.id}"
   end
 
 end
