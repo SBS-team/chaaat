4be3821417cec13bd1c8b59c24f1db77478975cd
Viktor Markevich <viktor.markevich@faceit.com.ua>
2014-05-12 15:38:31 +0300

    fixed merge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,12 @@
 module ApplicationHelper
   def avatar_url(user, foto_size)
     if user.avatar.present?
-<<<<<<< HEAD
       foto_size == 50 ? user.avatar : user.profile_avatar
-=======
-      if foto_size == 50
-        user.avatar
-      else
-        user.profile_avatar
-      end
->>>>>>> develop
     else
       Gravatar.new(user.email).image_url( size: foto_size, default: "http://cdn2.vox-cdn.com/images/verge/default-avatar.v9899025.gif?s=#{foto_size}" )
     end
   end
 
-<<<<<<< HEAD
   def get_user_status_style(status)
      { Available: 'glyphicon glyphicon-eye-open drop-av drop-col-mar',
        Away: 'glyphicon glyphicon-eye-close drop-away drop-col-mar',
@@ -33,21 +24,3 @@ module ApplicationHelper
   end
 
 end
-
-=======
-  def get_user_status_style(user_status)
-    case user_status
-      when "Available"
-        "glyphicon glyphicon-eye-open drop-av drop-col-mar"
-      when "Away"
-        "glyphicon glyphicon-eye-close drop-away drop-col-mar"
-      when "Do not disturb"
-        "glyphicon glyphicon-eye-close drop-dnd drop-col-mar"
-      when "Offline"
-        "glyphicon glyphicon-eye-close drop-col-mar"
-      when "Help"
-        "glyphicon glyphicon-question-sign drop-hlp drop-col-mar"
-    end
-  end
-end
->>>>>>> develop
