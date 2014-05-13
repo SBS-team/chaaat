a2dce9552e6f09e0dabeadd6b2b95e7c000cb09b
Viktor Markevich <viktor.markevich@faceit.com.ua>
2014-05-13 08:52:38 +0300

    text formating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,13 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def search
-    if params[:room_id].to_i>0
-      users = User.where("login like ? AND id != ? AND id NOT IN (?)", "%#{params[:login]}%", current_user.id, RoomsUser.where(:room_id => params[:room_id]).pluck(:user_id))
+    if params[:room_id].to_i > 0
+      user_id = RoomsUser.where( :room_id => params[:room_id] ).pluck( :user_id )
+      users = User.where( "login like ? AND id != ? AND id NOT IN (?)", "%#{params[:login]}%", current_user.id, user_id )
     else
-      users = User.where("login like ? AND id != ?", "%#{params[:login]}%", current_user.id)
+      users = User.where( "login like ? AND id != ?", "%#{params[:login]}%", current_user.id )
     end
-    render :json => users, :root => "users"
+    render json: users, root: "users"
   end
 
 
