9087d2df1e64d6ef482037cf0fcc8e8900b7d5cb
galas <artem.galas@gmail.com>
2014-05-20 16:00:02 +0300

    temp

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -6,9 +6,9 @@
 
 
 
-Pusher.app_id = '75414'
-Pusher.key    = 'af50ff06ce881179e1c6'
-Pusher.secret = 'df516d4610e1196537c1'
+#Pusher.app_id = '75414'
+#Pusher.key    = 'af50ff06ce881179e1c6'
+#Pusher.secret = 'df516d4610e1196537c1'
 Pusher.url = "http://af50ff06ce881179e1c6:df516d4610e1196537c1@api-eu.pusher.com/apps/75414"
 #Pusher.url = "http://255267aae6802ec7914f:456aa3a324e2b5e8a2f2@api.pusherapp.com/apps/68323"
 Pusher.logger = Rails.logger
