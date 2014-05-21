aac5663cde8cd30c180bbad9948f3180221e6f17
dmmoroka92 <dmoroka92@gmail.com>
2014-05-21 14:25:44 +0300

    Add build and coverage badges

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear!('rails')
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
