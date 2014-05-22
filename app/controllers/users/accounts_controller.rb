a09f042c59439bfd5eada393383d80e1ade8e246
lukjanenko <lukyan-i@yandex.ua>
2014-05-22 15:10:44 +0300

    fix twitter login

--- app/controllers/users/accounts_controller.rb
+++ app/controllers/users/accounts_controller.rb
@@ -3,11 +3,11 @@ class Users::AccountsController < ApplicationController
   def create
     data = session["devise.omniauth_data"]
     data[:email] = params[:user][:email]
-    @user = User.find_for_twitter_oauth(data, current_user)
+    @user = User.find_for_twitter_oauth(data)
     @user.email = data[:email]
     if @user.save
       flash[:notice] = I18n.t "devise.registrations.signed_up_but_unconfirmed"
-      redirect_to root_path
+      sign_in_and_redirect @user, :event => :authentication
     else
       flash[:error] = I18n.t "devise.omniauth_callbacks.failure", :kind => data[:provider].titleize, :reason => @user.errors.full_messages.first
       render "users/omniauth_callbacks/add_email"
