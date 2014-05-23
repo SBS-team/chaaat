352f045846ea61f18a5d1425480b439d2c31986f
dmmoroka92 <dmoroka92@gmail.com>
2014-05-23 13:28:59 +0300

    Coveralls testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,22 @@
-require 'coveralls'
-Coveralls.wear!('rails')
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-#require 'simplecov'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'email_spec'
 require 'rspec/autorun'
-#SimpleCov.start
+SimpleCov.start
 require 'shoulda-matchers'
 require 'capybara/rails'
 require 'capybara/rspec'
 require 'database_cleaner'
+
+require 'coveralls'
+Coveralls.wear!('rails')
+
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+require 'simplecov'
+
 
   include Devise::TestHelpers
 # Requires supporting ruby files with custom matchers and macros, etc,
