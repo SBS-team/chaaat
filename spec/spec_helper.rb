c82a8c67d9b41c4faf358a243a183c43abc32c2c
dmmoroka92 <dmoroka92@gmail.com>
2014-05-23 11:46:14 +0300

    Coveralls testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,8 @@
+require 'coveralls'
+Coveralls.wear!('rails')
+
 require 'codeclimate-test-reporter'
 require 'simplecov'
-require 'coveralls'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    Coveralls::SimpleCov::Formatter,
-    SimpleCov::Formatter::HTMLFormatter,
-    CodeClimate::TestReporter::Formatter
-]
-
-SimpleCov.start 'rails'
-Coveralls.wear!('rails')
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
