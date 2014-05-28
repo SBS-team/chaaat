c2a02c34df2079fae4c6651f84917ac251c769a2
Nataly <natalyasvd@gmail.com>
2014-05-28 13:59:23 +0300

    fix facebook heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, '5690879f7c91a6521a88', '9395adef4ad5487be40541b5fffa32a183ab30f3', :scope => 'user:email'
-  provider :facebook, '646226855446942', 'c5c2fcd0ffdf1a0728f590c83ffa344b'
+  provider :facebook, '297828783725765', '428ab150c0359303095b000331da9e86'
 end
