648d92b01154b7b6feb5288296ca6c7f4ecf9f04
Nataly <natalyasvd@gmail.com>
2014-06-03 15:21:47 +0300

    fix googleo_plus

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, '5690879f7c91a6521a88', '9395adef4ad5487be40541b5fffa32a183ab30f3', :scope => 'user:email'
   provider :facebook, '297828783725765', '428ab150c0359303095b000331da9e86'
+  provider :google_oauth2, "530975273735-9rd275aq2cirucpvdr8g1o8jom0mipsr.apps.googleusercontent.com", "7xtBDRtfJFQ8bn50zAE75fq3"
 end
